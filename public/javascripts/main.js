c1def6a80aa3e64b897d5ab8da7bee8c3fa4f872
Yeshen Shang <shangsunset@gmail.com>
2014-03-13 19:25:45 -0400

    fixed text wrapping and new linebreak in textarea

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,30 +1,27 @@
 // on load of page
 $(function(){
 
-    // when the client clicks SEND
-    $('.senddata').on('click', function(){
-        var message = $('.data').val();
-        $('.data').val('');
-        // tell server to execute 'sendChat' and send along one parameter
-        socket.emit('sendChat', message);
-    });
+
 
     // when the client hits ENTER on their keyboard
-    $('.data').keypress(function(e) {
+  $('.textContent').keypress(function(e) {
 
-      if (!$(".data").val()){
-      $(".senddata").attr("disabled", "disabled");
-      }
-      else {
-      $(".senddata").attr("enabled", "enabled");
-      
-        if(e.which === 13) {
+  if(e.which === 13) {
+    e.preventDefault();
 
-            $(this).blur();
-            $('.senddata').focus().click().blur();
-            $(this).focus();
-        }
-      }
+    if ($(".textContent").val().trim().length == 0){
+      $(".sendContent").attr("disabled", "disabled");
+    }
+    else{
+    $(".sendContent").attr("enabled", "enabled");
+      var message = $('.textContent').val();
+    $('.textContent').val('');
+    // tell server to execute 'sendChat' and send along one parameter
+    socket.emit('sendChat', message);
+  }
+            
+}  
+
  });
 
 });
