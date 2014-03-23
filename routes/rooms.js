6713df288f6c2f3177ffe1349a2ab5745b63fd16
Yeshen Shang <shangsunset@gmail.com>
2014-03-23 00:47:29 -0400

    user can be directed to video chat after creating a room

--- routes/rooms.js
+++ routes/rooms.js
@@ -14,11 +14,9 @@ exports.addRoom = function (req, res) {
 	res.render('addRoom');
 };
 
-// exports.rooms = function (req, res) {
-//  	res.render('rooms');
-// };
 
 exports.createRoom = function (req, res) {
 	db.addRooms({roomName: req.body.roomName});
-	res.redirect('/rooms');
+	req.session.roomName = req.body.roomName;
+	res.redirect('rooms/videochat/' + req.session.roomName);
 };
